01a29af950ea2f1762b0189ce22265ff126d15dd
luneto10 <lguedesdecarvalhon2@huskers.unl.edu>
2024-02-15 22:09:37 -0600

    Feat: Add Sale Class

---
+++ src/unl/soc/Sale.java
@@ -0,0 +1,30 @@
+package unl.soc;
+
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Random;
+
+public class Sale {
+
+
+    private String uniqueCode = generateSaleUniqueCode();
+    private Store store;
+    private Item item;
+    private Person salesman;
+    private LocalDateTime dateTime;
+
+    private String generateSaleUniqueCode() {
+
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        Random random = new Random();
+        StringBuilder stringBuilder = new StringBuilder(20);
+        for (int i = 0; i < 20; i++) {
+            int randomIndex = random.nextInt(characters.length());
+            stringBuilder.append(characters.charAt(randomIndex));
+        }
+        return stringBuilder.toString();
+    }
+}
+
