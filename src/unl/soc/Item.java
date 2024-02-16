af3f3fef1cecc5feabda2ca72615b2975c1e499f
lucascoelho_unl <lmoreiradesouzacoe2@huskers.unl.edu>
2024-02-15 21:36:14 -0600

    Import List & ArrayList

--- src/unl/soc/Item.java
+++ src/unl/soc/Item.java
@@ -1,7 +1,7 @@
 package unl.soc;
 
 public class Item {
-    private String uniquecCode;
+    private String uniqueCode;
     private String type;
     private String name;
     private double basePrice;
