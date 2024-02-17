63e5f1e5f0e8a2af497caebb69f98e75ec2b645c
luneto10 <lguedesdecarvalhon2@huskers.unl.edu>
2024-02-17 15:56:55 -0600

    Feat: Add documentation

--- src/unl/soc/Utils.java
+++ src/unl/soc/Utils.java
@@ -7,6 +7,14 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Utils {
+
+    /**
+     * Reads data from a CSV file and creates a list of Item objects.
+     *
+     * @param path The path to the CSV file.
+     * @return A List of Item objects read from the CSV file.
+     * @throws RuntimeException if the file is not found or if there is an issue reading the file.
+     */
     public static List<Item> readCSVItems(String path) {
         try {
             Scanner s = new Scanner(new File(path));
@@ -19,18 +27,28 @@ public class Utils {
                 Item item = new Item(itemsInfo.get(0), itemsInfo.get(1), itemsInfo.get(2), Double.parseDouble(itemsInfo.get(3)));
                 items.add(item);
             }
+            s.close();
             return items;
+
         } catch (FileNotFoundException e) {
             throw new RuntimeException("File not find on" + path + e);
         }
     }
 
+    /**
+     * Parses a list of Item objects into separate lists based on their types and constructs a map
+     * containing these lists categorized as product purchases, product leases, voice plans, data plans, and services.
+     *
+     * @param itemsList The list of Item objects to be parsed.
+     * @return A map containing categorized lists of ProductPurchase, ProductLease, VoicePlan, DataPlan, and Service objects.
+     */
     public static Map<String, Object> itemsDictParse(List<Item> itemsList) {
         List<ProductPurchase> productPurchaseList = new ArrayList<>();
         List<VoicePlan> voicePlanList = new ArrayList<>();
         List<DataPlan> dataPlanList = new ArrayList<>();
         List<ProductLease> productLeaseList = new ArrayList<>();
         List<Service> serviceList = new ArrayList<>();
+
         Map<String, Object> result = new HashMap<>(Map.of(
                 "purchase", productPurchaseList,
                 "lease", productLeaseList,
@@ -43,15 +61,18 @@ public class Utils {
                 ProductPurchase purchase = new ProductPurchase(item.getUniqueCode(), item.getType(), item.getName(), item.getBasePrice());
                 productPurchaseList.add(purchase);
                 result.put("purchase", productPurchaseList);
-            } else if (item.getType().compareTo("S") == 0) {
+            }
+            else if (item.getType().compareTo("S") == 0) {
                 Service service = new Service(item.getUniqueCode(), item.getType(), item.getName(), item.getBasePrice());
                 serviceList.add(service);
                 result.put("service", serviceList);
-            } else if (item.getType().compareTo("V") == 0) {
+            }
+            else if (item.getType().compareTo("V") == 0) {
                 VoicePlan voicePlan = new VoicePlan(item.getUniqueCode(), item.getType(), item.getName(), item.getBasePrice());
                 voicePlanList.add(voicePlan);
                 result.put("voicePlan", voicePlanList);
-            } else if (item.getType().compareTo("D") == 0) {
+            }
+            else if (item.getType().compareTo("D") == 0) {
                 DataPlan dataPlan = new DataPlan(item.getUniqueCode(), item.getType(), item.getName(), item.getBasePrice());
                 dataPlanList.add(dataPlan);
                 result.put("dataPlan", dataPlanList);
