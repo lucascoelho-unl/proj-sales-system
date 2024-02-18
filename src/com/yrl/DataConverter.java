529af6b46bfb057741a5c279781e01f891a6cb9a
lucascoelho_unl <lmoreiradesouzacoe2@huskers.unl.edu>
2024-02-18 00:01:16 -0600

    Feat: Add XML Data Convertor

--- src/com/yrl/DataConverter.java
+++ src/com/yrl/DataConverter.java
@@ -2,14 +2,15 @@ package com.yrl;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.thoughtworks.xstream.XStream;
+import unl.soc.items.Item;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class DataConverter {
-
-    public static void createJsonFile(Object object, String filePath){
+    public static void createJsonFile(Object object, String filePath) {
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String json = gson.toJson(object);
@@ -17,9 +18,20 @@ public class DataConverter {
             BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
             writer.write(json);
             writer.close();
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
+    }
+    public static void  createXMLFile(Object object, String filePath) {
+        XStream xStream = new XStream();
+        xStream.alias("Item", Item.class);
+        String xmlConversion = xStream.toXML(object);
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
+            writer.write(xmlConversion);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/unl/soc/teste.java
+++ src/unl/soc/teste.java
@@ -3,6 +3,7 @@ package unl.soc;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import com.thoughtworks.xstream.XStream;
 import com.yrl.DataConverter;
 import unl.soc.items.*;
 
@@ -13,9 +14,16 @@ public class teste {
     public static void main(String[] args){
         List<Item> teste = Utils.readCSVItems("data/Items.csv");
         Map<String, Object> teste2 = Utils.itemsDictParse(teste);
+
         System.out.println(teste);
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         var json = gson.toJson(teste);
+        System.out.println(json);
+
+        XStream xStream = new XStream();
+        xStream.alias("Item", Item.class);
+        String xmlConversion = xStream.toXML(teste);
+        System.out.println(xmlConversion);
 
         var personList = Utils.readCSVPerson("data/Persons.csv");
         System.out.println(personList);
