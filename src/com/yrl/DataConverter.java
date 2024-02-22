b79cf7ade562e53bc98c8da8e55410811fb74707
lucascoelho_unl <lmoreiradesouzacoe2@huskers.unl.edu>
2024-02-22 11:10:46 -0600

    Refact: Move functions from DataConvertor to Utils Refact: Removed packages in src for grader

--- src/com/yrl/DataConverter.java
+++ src/com/yrl/DataConverter.java
@@ -1,15 +1,6 @@
 package com.yrl;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.thoughtworks.xstream.XStream;
 import unl.soc.*;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 
 public class DataConverter {
     public static void main(String[] args) {
@@ -17,20 +8,21 @@ public class DataConverter {
         var itemMap = Utils.readItemsCSVtoMap("data/Items.csv");
         var itemList = Utils.readItemsCSVtoList("data/Items.csv");
 
-        Utils.createXMLFile(itemList, "data/itemTest.xml");
-        Utils.createJsonFile(itemList, "outputTest/ItemTestList.json");
+        Utils.createXMLFile(itemList, "data/itemOutput.xml");
+        Utils.createJsonFile(itemList, "data/ItemOutput.json");
 
         //Testing Person
         var personMap = Utils.readPersonCSVtoMap("data/Persons.csv");
         var personList = Utils.readPersonCSVtoList("data/Persons.csv");
 
-        Utils.createXMLFile(personList, "outputTest/PersonTest.xml");
-        Utils.createJsonFile(personList, "outputTest/PersonTestList.json");
+        Utils.createXMLFile(personList, "data/PersonOutput.xml");
+        Utils.createJsonFile(personList, "data/PersonOutput.json");
 
         //Testing Store
         var storeMap = Utils.readStoreCSVtoMap("data/Stores.csv");
         var storeList = Utils.readStoreCSVtoList("data/Stores.csv");
-        Utils.createXMLFile(storeList, "outputTest/StoreTest.xml");
-        Utils.createJsonFile(storeList, "outputTest/StoreTestList.json");
+
+        Utils.createXMLFile(storeList, "data/StoreOutput.xml");
+        Utils.createJsonFile(storeList, "data/StoreOutput.json");
     }
 }
