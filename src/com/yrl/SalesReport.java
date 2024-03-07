61c4edaf975a8480e0f7fd64410ef5320676fb02
lucascoelho_unl <lmoreiradesouzacoe2@huskers.unl.edu>
2024-03-07 13:39:54 -0600

    Refact: remove output file from main

--- src/com/yrl/SalesReport.java
+++ src/com/yrl/SalesReport.java
@@ -16,7 +16,6 @@ public class SalesReport {
      * The main method generates and prints the summary reports.
      */
     public static void main(String[] args) {
-        DataWriter.createSaleReportInNewFile("data/output.txt");
         DataWriter.printSalesReport();
     }
 }
