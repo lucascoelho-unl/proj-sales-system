afe73290afb8184cd0f55beb400732792e2e15a7
luneto10 <lguedesdecarvalhon2@huskers.unl.edu>
2024-04-07 01:55:01 -0500

    Feat: Start imolementing loading store from database.

--- src/unl/soc/DatabaseLoader.java
+++ src/unl/soc/DatabaseLoader.java
@@ -4,14 +4,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
 
 public class DatabaseLoader {
     public static void main(String[] args) {
-        Address test = loadAddress(1);
+        Address test = loadAddress(100);
         System.out.println(test);
     }
-
 
     public static Address loadAddress(int addressId) {
         Connection conn = ConnFactory.createConnection();
@@ -45,5 +45,62 @@ public class DatabaseLoader {
          return address;
     }
 
+    public static Map<Integer, Address> loadAllAddress(){
+        Connection conn = ConnFactory.createConnection();
+        PreparedStatement ps;
+        ResultSet rs;
+        Map<Integer, Address> addressMap = new HashMap<>();
 
+        String query = """
+                        select addressId from Address;
+                        """;
+        try{
+            ps = conn.prepareStatement(query);
+            rs = ps.executeQuery();
+            while (rs.next()){
+                Address address = loadAddress(rs.getInt("addressId"));
+                addressMap.put(rs.getInt("addressId"), address);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error in the connection: " + e);
+            throw new RuntimeException(e);
+        }
+
+        ConnFactory.closeConnection(rs, ps, conn);
+        return addressMap;
+    }
+
+    public static Store loadStore(int storeId) {
+        Connection conn = ConnFactory.createConnection();
+        PreparedStatement ps;
+        ResultSet rs;
+        Store store = null;
+
+        String query = """
+                        select s.storeCode, s.managerId, s.addressId from Store s
+                        where storeId = ?
+                        """;
+        try{
+            ps = conn.prepareStatement(query);
+            ps.setInt(1, storeId);
+            rs = ps.executeQuery();
+            if (rs.next()){
+                int storeCode = rs.getInt("storeCode");
+                int managerId = rs.getInt("managerId");
+                int addressId = rs.getInt("addressId");
+                Address address = loadAddress(addressId);
+                //TODO: Create a Person loader class to use here
+//                Person manager = loadPerson(managerId);
+//                store = new Store(storeId, storeCode, address, manager);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error in the connection: " + e);
+            throw new RuntimeException(e);
+        }
+
+        return store;
+    }
+
+    //TODO: Create the load person class.
 }
