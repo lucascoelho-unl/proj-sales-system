903146cdb5652b5aa7e5d555a605fa02d9362708
lucascoelho_unl <lmoreiradesouzacoe2@huskers.unl.edu>
2024-04-09 12:03:14 -0500

    Refact: Reports now support DB loading

--- src/com/yrl/SalesReport.java
+++ src/com/yrl/SalesReport.java
@@ -4,7 +4,7 @@ import unl.soc.DataReporter;
 
 /**
  * The SalesReport class generates a summary report of sales data.
- * It reads sales, items, persons, and store information from CSV files,
+ * It reads sales, items, persons, and stores information from CSV files,
  * processes the data, and prints a summary report including total items sold,
  * total tax collected, and total sales revenue.
  */
@@ -14,9 +14,9 @@ public class SalesReport {
      * The main method generates and prints the summary reports.
      */
     public static void main(String[] args) {
-        String totalsReport = DataReporter.reportTotalsBySales();
-        String storeTotals = DataReporter.reportTotalsByStore();
-        String salesReport = DataReporter.reportSales();
+        String totalsReport = DataReporter.reportTotalsBySalesFromDB();
+        String storeTotals = DataReporter.reportTotalsByStoreFromDB();
+        String salesReport = DataReporter.reportSalesFromDB();
 
         System.out.println(totalsReport);
         System.out.println(storeTotals);
