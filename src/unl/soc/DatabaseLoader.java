2990406afd5a70549ab551a87e98afa925405c30
lucascoelho_unl <lmoreiradesouzacoe2@huskers.unl.edu>
2024-04-25 22:24:16 -0500

    Refact: Database now has type in item

--- src/unl/soc/DatabaseLoader.java
+++ src/unl/soc/DatabaseLoader.java
@@ -345,7 +345,7 @@ public class DatabaseLoader {
     /**
      * Loads a Store object from the database based on the given store ID.
      *
-     * @param storeId The ID of the store to load.
+     * @param storeCode The ID of the store to load.
      * @return The Store object loaded from the database with its sales.
      */
 
@@ -410,9 +410,8 @@ public class DatabaseLoader {
         Item item = null;
 
         String query = """
-                select Item.itemId, uniqueCode, basePrice, name, type, startDate from Item
-                left join ItemSale on Item.itemId = ItemSale.itemId
-                where Item.itemId = ?;
+                select itemId, uniqueCode, basePrice, name, type from Item
+                where itemId = ?;
                 """;
 
         try {
@@ -430,15 +429,9 @@ public class DatabaseLoader {
                     case "S" -> new Service(itemId, uniqueCode, name, basePrice);
                     case "D" -> new DataPlan(itemId, uniqueCode, name, basePrice);
                     case "V" -> new VoicePlan(itemId, uniqueCode, name, basePrice);
+                    case "P" -> new ProductPurchase(itemId, uniqueCode, name, basePrice);
                     default -> null;
                 };
-
-                if (rs.getString("startDate") != null && type.equals("L")) {
-                    item = new ProductLease(itemId, uniqueCode, name, basePrice);
-                } else if (type.equals("P")) {
-                    item = new ProductPurchase(itemId, uniqueCode, name, basePrice);
-                }
-
             }
         } catch (SQLException e) {
             LOGGER.error("Error loading item {}: ", itemId, e);
@@ -501,11 +494,10 @@ public class DatabaseLoader {
         Item item = null;
 
         String query = """
-                select i.itemId, type, startDate, endDate, totalGb, totalHours, employeeId, totalPeriod, phoneNumber from Item i
+                select i.itemId, i.type, startDate, endDate, totalGb, totalHours, employeeId, totalPeriod, phoneNumber, isLease from Item i
                 left join ItemSale its on its.itemId = i.itemId
                 where its.itemSaleId = ?;
                 """;
-
         try {
             ps = conn.prepareStatement(query);
             ps.setInt(1, itemSaleId);
@@ -514,18 +506,18 @@ public class DatabaseLoader {
                 String type = rs.getString("type");
                 item = loadItem(rs.getInt("itemId"));
 
-                // Switch case to determine the type of item sale to correctly instantiate the item instance.
-                item = switch (type) {
-                    case "L" -> new ProductLease(itemSaleId, item, rs.getString("startDate"), rs.getString("endDate"));
-                    case "V" ->
-                            new VoicePlan(itemSaleId, item, rs.getString("phoneNumber"), rs.getDouble("totalPeriod"));
-                    case "S" ->
-                            new Service(itemSaleId, item, rs.getDouble("totalHours"), loadPerson(rs.getInt("employeeId")));
-                    case "D" -> new DataPlan(itemSaleId, item, rs.getDouble("totalGb"));
-                    default -> item;
-                };
+                if (type.equals("P") && rs.getBoolean("isLease")) {
+                    item = new ProductLease(itemSaleId, item, rs.getString("startDate"), rs.getString("endDate"));
+                } else {
+                    // Switch case to determine the type of item sale to correctly instantiate the item instance.
+                    item = switch (type) {
+                        case "V" -> new VoicePlan(itemSaleId, item, rs.getString("phoneNumber"), rs.getDouble("totalPeriod"));
+                        case "S" -> new Service(itemSaleId, item, rs.getDouble("totalHours"), loadPerson(rs.getInt("employeeId")));
+                        case "D" -> new DataPlan(itemSaleId, item, rs.getDouble("totalGb"));
+                        default -> item;
+                    };
+                }
             }
-
         } catch (SQLException e) {
             LOGGER.error("Error loading item sold {}: ", itemSaleId, e);
             throw new RuntimeException(e);
@@ -619,7 +611,7 @@ public class DatabaseLoader {
     /**
      * Loads a Sale object from the database based on the given sale ID.
      *
-     * @param saleId The ID of the sale to load.
+     * @param uniqueCode The ID of the sale to load.
      * @return The Sale object loaded from the database.
      */
     public static Sale loadSale(String uniqueCode) {
